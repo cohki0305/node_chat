18907e6f597d6a18462cb3e1bbcf3cf7a82f7342
cohki0305 <cohki0305@gmail.com>
2018-05-12 15:52:25 +0900

    broadcast event

--- server/server.js
+++ server/server.js
@@ -13,6 +13,18 @@ const io = socketIO(server)
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcom to the chat app',
+    createdAt: new Date().getTime()
+  })
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  })
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message)
     io.emit('newMessage', {
@@ -20,6 +32,12 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     })
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createAt: new Data().getTime()
+    // })
   })
 
   socket.on('disconnect', () => {
